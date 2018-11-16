f6b524e3ede8d3d40d4922e9401f12c563c6e212
brianquach123 <brianquach123@gmail.com>
2018-11-16 00:56:34 -0500

    skeleton for freewrite template, only has plain ui elements

--- app/src/main/java/com/example/neema/storyboard/HomepageActivity.java
+++ app/src/main/java/com/example/neema/storyboard/HomepageActivity.java
@@ -8,6 +8,6 @@ public class HomepageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_screen_OLDVERSION);
+        setContentView(R.layout.homescreen);
     }
 }
