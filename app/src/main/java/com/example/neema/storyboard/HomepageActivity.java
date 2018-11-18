b42ed4615f02a13b2d5600e331ff358213ee8079
Steven Sombat <sombatsteven@gmail.com>
2018-11-17 19:01:51 -0500

    Weekly challenges foundations created. Added a Card class to hold information about the cards. The database now has a "CardTable" that contains a table of Users. The Table of Users will have a list of Cards.

--- app/src/main/java/com/example/neema/storyboard/HomepageActivity.java
+++ app/src/main/java/com/example/neema/storyboard/HomepageActivity.java
@@ -1,7 +1,9 @@
 package com.example.neema.storyboard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class HomepageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
+    }
+
+    public void weeklyChallengeTap (View v) {
+        startActivity(new Intent(this, WeeklyChallengeActivity.class));
     }
 }
