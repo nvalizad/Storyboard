8e2c03079ff7bfaec94f785c2d31b24d3258b198
waynema <wma@umd.edu>
2018-11-30 00:50:34 -0500

    took out the message

--- app/src/main/java/com/example/neema/storyboard/CommentFragment.java
+++ app/src/main/java/com/example/neema/storyboard/CommentFragment.java
@@ -1,4 +1,26 @@
 package com.example.neema.storyboard;
+import android.graphics.Canvas;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
-public class CommentFragment {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommentFragment extends AppCompatActivity {
+    private CommunityCardAdapter mAdapter;
+    List<Card> communityCards = new ArrayList<>();
 }
